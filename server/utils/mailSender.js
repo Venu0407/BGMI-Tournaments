52732372a751b8e33e2347a64236a77c3ef6d22b
Venu Gopal Garg <venugopalgarg@gmail.com>
2025-11-05 23:31:12 +0530

    Update mailSender.js

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -15,7 +15,7 @@ const mailSender = async (email, title, body) => {
     })
 
     let info = await transporter.sendMail({
-      from: `"Maverix | Arvind Sharma" <${process.env.MAIL_USER}>`, // sender address
+      from: `"Maverix | Venu" <${process.env.MAIL_USER}>`, // sender address
       to: `${email}`, // list of receivers
       subject: `${title}`, // Subject line
       html: `${body}`, // html body
